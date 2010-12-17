8a19e05cd6a157812db7208af25badec18dc9843
for.sqven <for.sqven@58625a3b-5f24-8265-80b7-ccb988449c27>
2010-12-17 05:49:23 +0000

    1. 改變編譯設定檔, 改為完整封裝成 jar 2. 更新相關 lib 元件的版本到最新版 3. 修改兩種啟動起, 改為使用 jar 的方式啟動 (啟動類別&相關引用類別直接寫入在 jar 內)

--- ServerStart.bat
+++ ServerStart.bat
@@ -1,2 +1,4 @@
-@java -Xmx512m -Xincgc -cp l1jserver.jar;lib\c3p0-0.9.1.1.jar;lib\mysql-connector-java-5.1.5-bin.jar;lib\javolution.jar l1j.server.Server
-@pause
+@ECHO OFF
+java -Xmx512m -Xincgc -jar l1jserver.jar
+
+PAUSE
--- ServerStart.sh
+++ ServerStart.sh
@@ -6,4 +6,4 @@
 PATH=.:/usr/local/bin:/usr/bin:/bin:/usr/games:/usr/local/jdk1.7.0/bin
 export PATH
 
-java -Xmx512m -Xincgc -cp l1jserver.jar:./lib/c3p0-0.9.1.1.jar:./lib/javolution.jar:./lib/mysql-connector-java-5.1.5-bin.jar l1j.server.Server
+java -Xmx512m -Xincgc -jar l1jserver.jar
