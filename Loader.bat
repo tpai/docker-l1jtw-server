042aaee7f34ba6adc183a04c88baef0aa82db9cd
for.sqven <for.sqven@58625a3b-5f24-8265-80b7-ccb988449c27>
2011-03-27 10:46:39 +0000

    1. 增加可自行設定 JAVA 啟動參數的啟動器

---
+++ Loader.bat
@@ -0,0 +1,1 @@
+@start java -server -Xmx8m -Xms8m -jar l1jloader.jar
---
+++ Loader.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#PATH=.:/usr/local/bin:/usr/bin:/bin
+#export PATH
+
+java -server -Xmx8m -Xms8m -jar l1jloader.jar
