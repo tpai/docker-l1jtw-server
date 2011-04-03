46eaa661ab01eda196c354543b835dd834f0f597
intel_core_i9@yahoo.com.tw <intel_core_i9@yahoo.com.tw@58625a3b-5f24-8265-80b7-ccb988449c27>
2011-04-03 15:49:49 +0000

    S_ActiveSpells.java 封包修正

--- src/l1j/server/server/serverpackets/S_ActiveSpells.java
+++ src/l1j/server/server/serverpackets/S_ActiveSpells.java
@@ -28,43 +28,6 @@ public class S_ActiveSpells extends ServerBasePacket {
 		randBox[0] = Random.nextByte();
 		randBox[1] = Random.nextByte();
 
-		int[] time = new int[101];
-
-		writeC(Opcodes.S_OPCODE_ACTIVESPELLS);
-		writeC(0x14);
-
-		if (pc.hasSkillEffect(4001)) { // 神力藥水150%
-			time[42] = 900 / 4; // 該技能最後登出時間
-			UByte8[42] = time[42]; // 圖示時間 = 時間 * 4 - 2
-			UByte8[43] = 32; // type 32: 狩獵的經驗值將會增加。
-			pc.setSkillEffect(4001, time[42] - 2);
-		} else if (pc.hasSkillEffect(4002)) { // 神力藥水175%
-			time[42] = 900 / 4; // 該技能最後登出時間
-			UByte8[42] = time[42]; // 圖示時間 = 時間 * 4 - 2
-			UByte8[43] = 33; // type 33: 狩獵的經驗值將會增加。
-			pc.setSkillEffect(4002, time[42] - 2);
-		} else if (pc.hasSkillEffect(4003)) { // 神力藥水200%
-			time[42] = 900 / 4; // 該技能最後登出時間
-			UByte8[42] = time[42]; // 圖示時間 = 時間 * 4 - 2
-			UByte8[43] = 34; // type 34: 狩獵的經驗值將會增加。
-			pc.setSkillEffect(4003, time[42] - 2);
-		} else if (pc.hasSkillEffect(4004)) { // 神力藥水225%
-			time[42] = 900 / 4; // 該技能最後登出時間
-			UByte8[42] = time[42]; // 圖示時間 = 時間 * 4 - 2
-			UByte8[43] = 35; // type 35: 狩獵的經驗值將會增加。
-			pc.setSkillEffect(4004, time[42] - 2);
-		} else if (pc.hasSkillEffect(4005)) { // 神力藥水250%
-			time[42] = 900 / 4; // 該技能最後登出時間
-			UByte8[42] = time[42]; // 圖示時間 = 時間 * 4 - 2
-			UByte8[43] = 36; // type 36: 狩獵的經驗值將會增加。
-			pc.setSkillEffect(4005, time[42] - 2);
-		} else if (pc.hasSkillEffect(4006)) { // 象牙塔妙藥
-			time[42] = 900 / 4; // 該技能最後登出時間
-			UByte8[42] = time[42]; // 圖示時間 = 時間 * 4 - 2
-			UByte8[43] = 54; // type 54:因為妙藥，身心都很輕鬆。提升體力回復量和魔力回復量。
-			pc.setSkillEffect(4006, time[42] - 2);
-		}
-
 		writeC(Opcodes.S_OPCODE_ACTIVESPELLS);
 		writeC(0x14);
 
