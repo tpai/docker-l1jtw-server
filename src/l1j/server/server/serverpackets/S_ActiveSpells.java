679a0de5019e67d2ba4082b1e0a5252dd5b0cb50
intel_core_i9@yahoo.com.tw <intel_core_i9@yahoo.com.tw@58625a3b-5f24-8265-80b7-ccb988449c27>
2011-04-08 14:11:29 +0000

    補充Rev.85遺漏檔案。

--- src/l1j/server/server/serverpackets/S_ActiveSpells.java
+++ src/l1j/server/server/serverpackets/S_ActiveSpells.java
@@ -21,6 +21,10 @@ import static l1j.server.server.model.skill.L1SkillId.EFFECT_POTION_OF_EXP_200;
 import static l1j.server.server.model.skill.L1SkillId.EFFECT_POTION_OF_EXP_225;
 import static l1j.server.server.model.skill.L1SkillId.EFFECT_POTION_OF_EXP_250;
 import static l1j.server.server.model.skill.L1SkillId.EFFECT_BLESS_OF_MAZU;
+import static l1j.server.server.model.skill.L1SkillId.EFFECT_POTION_OF_BATTLE;
+import static l1j.server.server.model.skill.L1SkillId.EFFECT_STRENGTHENING_HP;
+import static l1j.server.server.model.skill.L1SkillId.EFFECT_STRENGTHENING_MP;
+import static l1j.server.server.model.skill.L1SkillId.EFFECT_ENCHANTING_BATTLE;
 
 import l1j.server.server.Opcodes;
 import l1j.server.server.datatables.CharBuffTable;
@@ -59,28 +63,33 @@ public class S_ActiveSpells extends ServerBasePacket {
 		if (pc.hasSkillEffect(STATUS_RIBRAVE)) {
 			data[61] = pc.getSkillEffectTimeSec(STATUS_RIBRAVE) / 4;
 		}
-		 // 神力藥水
-		if (pc.hasSkillEffect(EFFECT_POTION_OF_EXP_250)) { // 神力藥水250%
+		// 戰鬥藥水、神力藥水
+		if (pc.hasSkillEffect(EFFECT_POTION_OF_BATTLE)) {
+			data[45] = pc.getSkillEffectTimeSec(EFFECT_POTION_OF_BATTLE) / 16;
+			if (data[45] != 0) {
+				data[62] = 20; // 經驗值加成20%。
+			}
+		} else if (pc.hasSkillEffect(EFFECT_POTION_OF_EXP_250)) {
 			data[42] = pc.getSkillEffectTimeSec(EFFECT_POTION_OF_EXP_250) / 4;
 			if (data[42] != 0) {
 				data[43] = 36; // 狩獵經驗值將會增加。
 			}
-		} else if (pc.hasSkillEffect(EFFECT_POTION_OF_EXP_225)) { // 神力藥水225%
+		} else if (pc.hasSkillEffect(EFFECT_POTION_OF_EXP_225)) {
 			data[42] = pc.getSkillEffectTimeSec(EFFECT_POTION_OF_EXP_225) / 4;
 			if (data[42] != 0) {
 				data[43] = 35; // 狩獵經驗值將會增加。
 			}
-		} else if (pc.hasSkillEffect(EFFECT_POTION_OF_EXP_200)) { // 神力藥水200%
+		} else if (pc.hasSkillEffect(EFFECT_POTION_OF_EXP_200)) {
 			data[42] = pc.getSkillEffectTimeSec(EFFECT_POTION_OF_EXP_200) / 4;
 			if (data[42] != 0) {
 				data[43] = 34; // 狩獵經驗值將會增加。
 			}
-		} else if (pc.hasSkillEffect(EFFECT_POTION_OF_EXP_175)) { // 神力藥水175%
+		} else if (pc.hasSkillEffect(EFFECT_POTION_OF_EXP_175)) {
 			data[42] = pc.getSkillEffectTimeSec(EFFECT_POTION_OF_EXP_175) / 4;
 			if (data[42] != 0) {
 				data[43] = 33; // 狩獵經驗值將會增加。
 			}
-		} else if (pc.hasSkillEffect(EFFECT_POTION_OF_EXP_150)) { // 神力藥水150%
+		} else if (pc.hasSkillEffect(EFFECT_POTION_OF_EXP_150)) {
 			data[42] = pc.getSkillEffectTimeSec(EFFECT_POTION_OF_EXP_150) / 4;
 			if (data[42] != 0) {
 				data[43] = 32; // 狩獵經驗值將會增加。
@@ -91,6 +100,23 @@ public class S_ActiveSpells extends ServerBasePacket {
 			data[48] = pc.getSkillEffectTimeSec(EFFECT_BLESS_OF_MAZU) / 16;
 			if (data[48] != 0) {
 				data[49] = 44; // 感受到媽祖的祝福。
+			}
+		}
+		// 體力增強卷軸、魔力增強卷軸、強化戰鬥卷軸
+		if (pc.hasSkillEffect(EFFECT_STRENGTHENING_HP)) {
+			data[46] = pc.getSkillEffectTimeSec(EFFECT_STRENGTHENING_HP) / 16;
+			if (data[46] != 0) {
+				data[47] = 0; // 體力上限+50，體力回復+4。
+			}
+		} else if (pc.hasSkillEffect(EFFECT_STRENGTHENING_MP)) {
+			data[46] = pc.getSkillEffectTimeSec(EFFECT_STRENGTHENING_MP) / 16;
+			if (data[46] != 0) {
+				data[47] = 1; // 魔力上限+40，魔力回復+4。
+			}
+		} else if (pc.hasSkillEffect(EFFECT_ENCHANTING_BATTLE)) {
+			data[46] = pc.getSkillEffectTimeSec(EFFECT_ENCHANTING_BATTLE) / 16;
+			if (data[46] != 0) {
+				data[47] = 2; // 攻擊成功及攻擊力+3,魔攻+3,遠距離攻擊及命中率+3。
 			}
 		}
 		return data;
