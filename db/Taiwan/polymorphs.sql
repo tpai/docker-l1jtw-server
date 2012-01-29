ad390880da36b075d6e799b71864b43f8bbc96d3
ibm99nets <ibm99nets@58625a3b-5f24-8265-80b7-ccb988449c27>
2012-01-29 08:52:13 +0000

    1.polymorphs變身清單更新到3.51C   [ 團員: attendant ]

--- db/Taiwan/polymorphs.sql
+++ db/Taiwan/polymorphs.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50140
 File Encoding         : 65001
 
-Date: 2011-02-04 06:56:00
+Date: 2012-01-29 16:50:00
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -34,7 +34,7 @@ CREATE TABLE `polymorphs` (
 -- ----------------------------
 INSERT INTO `polymorphs` VALUES ('29', 'floating eye', '29', '1', '0', '0', '1', '7');
 INSERT INTO `polymorphs` VALUES ('95', 'shelob', '95', '10', '0', '0', '0', '7');
-INSERT INTO `polymorphs` VALUES ('146', 'ungoliant', '146', '10', '0', '0', '0', '7');
+INSERT INTO `polymorphs` VALUES ('146', 're ungoliant', '146', '1', '0', '0', '0', '7');
 INSERT INTO `polymorphs` VALUES ('938', 'beagle', '938', '1', '0', '2', '1', '7');
 INSERT INTO `polymorphs` VALUES ('945', 'milkcow', '945', '1', '0', '0', '0', '7');
 INSERT INTO `polymorphs` VALUES ('947', 'deer', '947', '1', '0', '0', '0', '7');
@@ -46,7 +46,7 @@ INSERT INTO `polymorphs` VALUES ('1047', 'scorpion', '1047', '15', '0', '0', '0'
 INSERT INTO `polymorphs` VALUES ('2064', 'snowman', '2064', '1', '0', '1027', '1', '7');
 INSERT INTO `polymorphs` VALUES ('2284', 'dark elf polymorph', '2284', '52', '256', '4095', '1', '7');
 INSERT INTO `polymorphs` VALUES ('2323', 'orc scout polymorph', '2323', '15', '256', '4095', '1', '7');
-INSERT INTO `polymorphs` VALUES ('2374', 'skeleton polymorph', '2374', '10', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('2374', 're skeleton', '2374', '1', '751', '4095', '1', '7');
 INSERT INTO `polymorphs` VALUES ('2376', 'skeleton axeman polymorph', '2376', '10', '1791', '4095', '1', '7');
 INSERT INTO `polymorphs` VALUES ('2377', 'skeleton pike polymorph', '2377', '10', '1791', '4095', '1', '7');
 INSERT INTO `polymorphs` VALUES ('2378', 'spartoi polymorph', '2378', '10', '751', '4095', '1', '7');
@@ -62,7 +62,7 @@ INSERT INTO `polymorphs` VALUES ('3103', 'barlog morph', '3103', '52', '1791', '
 INSERT INTO `polymorphs` VALUES ('3126', 'fire bowman morph', '3126', '51', '256', '4095', '1', '7');
 INSERT INTO `polymorphs` VALUES ('3630', 'cyclops', '3630', '40', '751', '4095', '1', '7');
 INSERT INTO `polymorphs` VALUES ('3631', 'griffon', '3631', '40', '0', '32', '1', '7');
-INSERT INTO `polymorphs` VALUES ('3632', 'cockatrice', '3632', '40', '0', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('3632', 're cockatrice', '3632', '1', '0', '4095', '1', '7');
 INSERT INTO `polymorphs` VALUES ('3633', 'ettin', '3633', '40', '751', '4095', '1', '7');
 INSERT INTO `polymorphs` VALUES ('3634', 'assassin', '3634', '45', '751', '4095', '1', '7');
 INSERT INTO `polymorphs` VALUES ('3664', 'baranka', '3664', '1', '704', '4095', '1', '7');
@@ -71,18 +71,18 @@ INSERT INTO `polymorphs` VALUES ('3784', 'death knight', '3784', '52', '751', '4
 INSERT INTO `polymorphs` VALUES ('3860', 'bow orc', '3860', '1', '256', '4095', '1', '7');
 INSERT INTO `polymorphs` VALUES ('3861', 'goblin', '3861', '1', '751', '913', '0', '7');
 INSERT INTO `polymorphs` VALUES ('3862', 'kobolds', '3862', '1', '751', '913', '1', '7');
-INSERT INTO `polymorphs` VALUES ('3863', 'dwarf', '3863', '1', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('3863', 're dwarf', '3863', '1', '751', '4095', '1', '7');
 INSERT INTO `polymorphs` VALUES ('3864', 'orc fighter', '3864', '1', '751', '4095', '1', '7');
-INSERT INTO `polymorphs` VALUES ('3865', 'werewolf', '3865', '1', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('3865', 're werewolf', '3865', '1', '751', '4095', '1', '7');
 INSERT INTO `polymorphs` VALUES ('3866', 'gandi orc', '3866', '10', '751', '4095', '1', '7');
 INSERT INTO `polymorphs` VALUES ('3867', 'rova orc', '3867', '10', '751', '4095', '1', '7');
 INSERT INTO `polymorphs` VALUES ('3868', 'atuba orc', '3868', '10', '751', '4095', '1', '7');
 INSERT INTO `polymorphs` VALUES ('3869', 'dudamara orc', '3869', '10', '1791', '4095', '1', '7');
 INSERT INTO `polymorphs` VALUES ('3870', 'neruga orc', '3870', '10', '1791', '4095', '1', '7');
-INSERT INTO `polymorphs` VALUES ('3871', 'skeleton archer polymorph', '3871', '10', '256', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('3871', 're skeleton archer', '3871', '1', '256', '4095', '1', '7');
 INSERT INTO `polymorphs` VALUES ('3872', 'zombie', '3872', '10', '751', '4095', '1', '7');
-INSERT INTO `polymorphs` VALUES ('3873', 'ghoul', '3873', '10', '751', '4095', '1', '7');
-INSERT INTO `polymorphs` VALUES ('3874', 'lycanthrope', '3874', '10', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('3873', 're ghoul', '3873', '10', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('3874', 're lycanthrope', '3874', '1', '751', '4095', '1', '7');
 INSERT INTO `polymorphs` VALUES ('3875', 'ghast', '3875', '10', '751', '945', '1', '7');
 INSERT INTO `polymorphs` VALUES ('3876', 'bugbear', '3876', '10', '751', '945', '1', '7');
 INSERT INTO `polymorphs` VALUES ('3877', 'ogre', '3877', '15', '1791', '913', '1', '7');
@@ -114,9 +114,9 @@ INSERT INTO `polymorphs` VALUES ('3902', 'Kelenis Morph', '3902', '1', '43', '40
 INSERT INTO `polymorphs` VALUES ('3903', 'Ken Lauhel Morph', '3903', '1', '751', '4095', '1', '7');
 INSERT INTO `polymorphs` VALUES ('3904', 'stone golem', '3904', '1', '751', '145', '1', '7');
 INSERT INTO `polymorphs` VALUES ('3905', 'beleth', '3905', '50', '751', '954', '1', '7');
-INSERT INTO `polymorphs` VALUES ('3906', 'orc', '3906', '1', '751', '4095', '1', '7');
-INSERT INTO `polymorphs` VALUES ('3945', 'gelatincube', '3945', '15', '751', '0', '1', '7');
-INSERT INTO `polymorphs` VALUES ('3950', 'middle oum', '3950', '15', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('3906', 're orc', '3906', '1', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('3945', 're gelatincube', '3945', '1', '751', '0', '1', '7');
+INSERT INTO `polymorphs` VALUES ('3950', 're middle oum', '3950', '1', '751', '4095', '1', '7');
 INSERT INTO `polymorphs` VALUES ('3952', 'vampire', '3952', '1', '0', '32', '0', '7');
 INSERT INTO `polymorphs` VALUES ('4000', 'knight vald morph', '4000', '1', '1791', '4095', '1', '7');
 INSERT INTO `polymorphs` VALUES ('4001', 'iris morph', '4001', '1', '1791', '4095', '1', '7');
@@ -274,3 +274,58 @@ INSERT INTO `polymorphs` VALUES ('7339', 'spearm 60', '7339', '60', '1080', '409
 INSERT INTO `polymorphs` VALUES ('7340', 'spearm 65', '7340', '65', '1080', '4095', '1', '7');
 INSERT INTO `polymorphs` VALUES ('7341', 'spearm 70', '7341', '70', '1080', '4095', '1', '7');
 INSERT INTO `polymorphs` VALUES ('8719', '柑橘', '8719', '1', '2047', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8774', 'young kelenis', '8774', '75', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8812', 'young gunter', '8812', '80', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8817', 'young ken lauel', '8817', '75', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8851', 'young dantes', '8851', '75', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8900', 'young helvine', '8900', '75', '256', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8913', 'young gilian', '8913', '80', '256', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8978', 'young bluedica', '8978', '80', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('9003', 'young joewoo', '9003', '80', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('9011', 'spearm 75', '9011', '75', '1080', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('9012', 'spearm 80', '9012', '80', '1080', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('9205', 'death 75', '9205', '75', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('9206', 'death 80', '9206', '80', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('9225', 'darkelf 75', '9225', '75', '256', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('9226', 'darkelf 80', '9226', '80', '256', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8860', 're orc scout', '8860', '1', '256', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8859', 're bugbear', '8859', '1', '751', '945', '1', '7');
+INSERT INTO `polymorphs` VALUES ('2437', 're giant ant', '2437', '1', '0', '136', '0', '7');
+INSERT INTO `polymorphs` VALUES ('2438', 're giant ant soldier', '2438', '1', '0', '136', '0', '7');
+INSERT INTO `polymorphs` VALUES ('8858', 're ghast', '8858', '1', '751', '945', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8865', 're lizardman', '8865', '1', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8866', 're kiwi parrot', '8866', '1', '0', '954', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8868', 're wild fang', '8868', '1', '0', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8863', 're raccoon', '8863', '1', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8864', 're owlbear', '8864', '1', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8867', 're ratman', '8867', '1', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8862', 're dragon fly', '8862', '1', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8861', 're succubus', '8861', '1', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8781', 're guard sword', '8781', '30', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8782', 're darkelf carrier', '8782', '30', '1080', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8783', 're darkelf assassin', '8783', '30', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8784', 're elder', '8784', '30', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8785', 're orc axe', '8785', '30', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8786', 're bandit bow', '8786', '30', '256', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8787', 're guard chief', '8787', '40', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8788', 're guard spear', '8788', '40', '1080', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8789', 're hose', '8789', '40', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8790', 're dark elder', '8790', '40', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8791', 're bandit axe', '8791', '40', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8792', 're darkelf ranger', '8792', '40', '256', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8793', 're cargo', '8793', '45', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8794', 're darkelf spear', '8794', '45', '1080', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8795', 're blaze', '8795', '45', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8796', 're black wizard', '8796', '45', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8797', 're dwarf boss', '8797', '45', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8798', 're darkelf thief', '8798', '45', '256', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8799', 're black knight chief', '8799', '50', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8800', 're black knight', '8800', '50', '1080', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8801', 're assassin master', '8801', '50', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8802', 're baphomet', '8802', '50', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8803', 're minotaur', '8803', '50', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8804', 're darkelf guard', '8804', '50', '256', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8805', 're dreik', '8805', '52', '192', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8806', 're xinclair', '8806', '52', '751', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8807', 're great minotaur', '8807', '52', '8', '4095', '1', '7');
+INSERT INTO `polymorphs` VALUES ('8808', 're darkelf bow', '8808', '52', '256', '4095', '1', '7');
